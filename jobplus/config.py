1c7192b7d067871f0d216f8e138a1431cfd9382f
speedxcc <943512835@qq.com>
2018-07-30 21:30:05 +0800

    update0730

--- jobplus/config.py
+++ jobplus/config.py
@@ -1,20 +1,19 @@
 class BaseConfig(object):
-    """ éç½®åºç±» """
-    SECRET_KEY = 'makesure to set a very secret key'
 
+    SECRET_KEY = 'makesure to set a very secret key'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/jobplus?charset=utf8'
 class DevelopmentConfig(BaseConfig):
-    """ å¼åç¯å¢éç½® """
+
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/simpledu?charset=utf8'
+ 
 
 
 class ProductionConfig(BaseConfig):
-    """ çäº§ç¯å¢éç½® """
+
     pass
 
 
 class TestingConfig(BaseConfig):
-    """ æµè¯ç¯å¢éç½® """
     pass
 
 
